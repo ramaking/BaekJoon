24347cfafe10de67f75e9d1cab6996ffc351a3ca
정규성 <aparter01@naver.com>
2023-01-18 18:00:03 +0900

    add

---
+++ src/acmicpc/Main1043.java
@@ -0,0 +1,30 @@
+package acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main1043 {
+	
+
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String line = br.readLine();
+    	
+    	StringTokenizer st = new StringTokenizer(line);
+
+    	int N = Integer.parseInt(st.nextToken()) ;
+    	int M = Integer.parseInt(st.nextToken()) ;
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+
+}
