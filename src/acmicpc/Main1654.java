1660aa7403a2b7ce9aaabd286c6ab62091ac18b9
ramaking <aparter01@naver.com>
2023-01-19 21:53:36 +0900

    Solve 1654, 1920

---
+++ src/acmicpc/Main1654.java
@@ -0,0 +1,79 @@
+package acmicpc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1654 {
+
+	public static int[] arr;
+
+	public static long highter;
+
+	public static int K;
+
+	public static long N;
+
+	public static long binarySearch(){
+		
+		long lower = 1;
+		// highter = arr.length-1;
+		long middle = (lower + highter)/2;
+
+		int sum = 0;
+
+		while(lower <= highter){
+			// System.out.println(middle);
+
+			for(int j = 0; j < K; j++){
+				sum += arr[j]/middle;
+			}
+
+			if(sum < N){
+				highter = middle-1;
+			} 
+
+			else if(sum >= N){
+				lower = middle+1;
+			}
+
+			middle = (lower + highter)/2;
+
+			sum = 0;
+		}
+
+		return middle;
+	}
+	
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+
+		K = sc.nextInt();
+
+		N = sc.nextInt();
+		
+		arr = new int[K];
+
+		long sum = 0;
+
+		for(int i = 0; i < K; i++){
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+
+		highter = sum/N;
+		// System.out.println(highter);
+
+
+
+		// Arrays.sort(arr);
+
+		System.out.println(binarySearch());
+		
+
+
+
+    	
+    }
+
+}
