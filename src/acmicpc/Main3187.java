f394e96fb7ef864591a772bef1dc60a868f78040
ramaking <aparter01@naver.com>
2023-01-25 00:28:18 +0900

    Try 6236

--- src/acmicpc/Main3187.java
+++ src/acmicpc/Main3187.java
@@ -15,68 +15,66 @@ public class Main3187 {
 	public static int R;
 	public static int C;
 
-	public static int sheepCount = 0;
-	public static int wolfCount = 0;
+	public static int sheepCount;
+	public static int wolfCount;
 
 	public static char[][] arr;
+    static boolean[][] visit;
 	
-	static int sheep = 0;
-	static int wolf = 0;
+	static int sheep;
+	static int wolf;
 	
 	static Queue<int[]> queue = new LinkedList<>();
+	// static Queue<Point> queue;
 
 	public static void bfs(int indexI, int indexJ) {
-		
-
-		int[] index = { indexI, indexJ };
-		queue.offer(index);
 
 		sheep = 0;
 		wolf = 0;
 
-		int curI;
-		int curJ;
-		int nextI;
-		int nextJ;
+		if (arr[indexI][indexJ] == 'v') {
+            wolf++;
+        } else {
+            sheep++;
+        }
 
-		while (true) {
-			if (queue.isEmpty()) {
-				if (sheep > wolf) {
-					sheepCount += sheep;
-				} else {
-					wolfCount += wolf;
-				}
-				break;
-			}
-			
-			index = queue.poll();
+		visit[indexI][indexJ] = true;
+		int[] index = {indexI, indexJ};
+		queue.offer(index);
 
-			curI = index[0];
-			curJ = index[1];
-			
-			// .이라면
-			if (arr[curI][curJ] == '.') {
-				arr[curI][curJ] = '#';
-			} else if (arr[curI][curJ] == 'k') {
-				sheep++;
-				arr[curI][curJ] = '#';
-			} else if (arr[curI][curJ] == 'v') {
-				wolf++;
-				arr[curI][curJ] = '#';
-			}
+		while (!queue.isEmpty()) {
+			// System.out.println(sheep + " " + wolf);
+
+			int[] p = queue.poll();
 
 			for (int i = 0; i < 4; i++) {
-				nextI = curI + di[i];
-				nextJ = curJ + dj[i];
+				int nextI = p[0] + di[i];
+				int nextJ = p[1] + dj[i];
+
+				if(nextI < 0 || nextJ < 0 || nextI >= R || nextJ >= C)
+					continue;
 
 				// # 이 아니라면
-				if (nextI >= 0 && nextI < R && nextJ >= 0 && nextJ < C && arr[nextI][nextJ] != '#') {
-					int[] nextIndex = {nextI, nextJ};
-					queue.offer(nextIndex);
+				if ( !visit[nextI][nextJ] &&  arr[nextI][nextJ] != '#') {
+					int[] nIndex = {nextI, nextJ};
+					queue.offer(nIndex);
 
+					visit[nextI][nextJ] = true;
+
+					if (arr[nextI][nextJ] == 'k') {
+						sheep++;
+						// arr[curI][curJ] = shap;
+					} else if (arr[nextI][nextJ] == 'v') {
+						wolf++;
+						// arr[curI][curJ] = shap;
+					}
 				}
-
 			}
+		}
+		if (sheep > wolf) {
+			sheepCount += sheep;
+		} else {
+			wolfCount += wolf;
 		}
 	}
 
@@ -84,28 +82,29 @@ public class Main3187 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String line = br.readLine();
+		// String line = 
 
-		StringTokenizer st = new StringTokenizer(line);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
 		arr = new char[R][C];
+        visit = new boolean[R][C];
 
 		for (int i = 0; i < R; i++) {
-			line = br.readLine();
+			String line = br.readLine();
 
 			for (int j = 0; j < C; j++) {
 				arr[i][j] = line.charAt(j);
 			}
 		}
 		
-		
+		queue = new LinkedList<>();
 
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				if (arr[i][j] == 'k' || arr[i][j] == 'v') {
+				if ((arr[i][j] == 'k' || arr[i][j] == 'v') && !visit[i][j]) {
 					bfs(i, j);
 				}
 			}
@@ -114,5 +113,15 @@ public class Main3187 {
 		System.out.println(sheepCount + " " + wolfCount);
 
 	}
+
+	static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
 }
