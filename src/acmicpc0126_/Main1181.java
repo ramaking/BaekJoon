ca2d253b035fbd68d5f7b8b607ad275e4f5be046
정규성 <aparter01@naver.com>
2023-01-26 16:09:48 +0900

    Create directory

---
+++ src/acmicpc0126_/Main1181.java
@@ -0,0 +1,21 @@
+package acmicpc0126_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1181 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = br.readLine();
+		
+		int N = Integer.parseInt(line);
+		
+		for(int i = 0; i < N; i++) {
+			line = br.readLine();
+		}
+		
+		
+	}
+}
