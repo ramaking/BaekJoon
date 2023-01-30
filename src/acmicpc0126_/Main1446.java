85d986a8080ef61ba5e783ac681b464b021e5ace
Gyusung <aparter01@naver.com>
2023-01-30 18:00:47 +0900

    test commit

---
+++ src/acmicpc0126_/Main1446.java
@@ -0,0 +1,23 @@
+package acmicpc0126_;
+
+import java.util.Scanner;
+
+public class Main1446 {
+
+	static int N;
+	
+	static int totalLength;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		totalLength = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			
+		}
+
+		sc.close();
+	}
+}
