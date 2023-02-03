3e81eb26973afaeb5742559cdf313ea6d2b63e3c
Gyusung <aparter01@naver.com>
2023-02-03 17:58:55 +0900

    solve 1504, try 1865

---
+++ src/acmicpc0126_/Main1865.java
@@ -0,0 +1,28 @@
+package acmicpc0126_;
+
+import java.util.Scanner;
+
+public class Main1865 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int tc = sc.nextInt();
+		
+		for(int t = 0; t < tc; t++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int w = sc.nextInt();
+			
+		}
+		
+		sc.close();
+	}
+	
+	public static boolean bellman() {
+		
+		
+		
+		
+		return true;
+	}
+}
---
+++ src/test/Main2.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main2 {
+	
+	class Parent{
+		
+	}
+	
+	class Chile extends Parent {
+		
+	}
+
+	static public void sow1() {
+
+	}
+
+	public static void sow2() {
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3 };
+		int[] arr2 = new int[] { 1, 2, 3 };
+		int[] arr3;
+		arr3 = new int[] { 1, 2, 3, 3 };
+
+		int[][] arr4 = new int[][] { { 1, 2, 3 } };
+		int[][] arr5 = new int[4][];
+		
+	}
+
+}
