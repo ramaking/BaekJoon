d38d87193e74c9ccf8c7186474d5bb082b7baa7e
Gyusung <aparter01@naver.com>
2023-02-08 17:51:29 +0900

    solve 15625

---
+++ src/acmicpc0208/Main15652.java
@@ -0,0 +1,49 @@
+package acmicpc0208;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+
+	static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static int[] numbers;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		
+		m = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[m];
+
+		combRepe(0, 1);
+
+		System.out.println(sb.toString());
+	}
+	
+	static void combRepe(int cnt, int start) {
+		
+		if(cnt == m) {
+			for(int i = 0; i < m; i ++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i <= n; i++) {
+			numbers[cnt] = i;
+			combRepe(cnt+1,i);
+			
+		}
+	}
+
+}
