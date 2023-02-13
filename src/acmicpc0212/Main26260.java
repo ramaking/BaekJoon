8e5de9c89bcb3574f9b98d8e99841482670a1220
ramaking <aparter01@naver.com>
2023-02-13 23:31:52 +0900

    solve 26260

--- src/acmicpc0212/Main26260.java
+++ src/acmicpc0212/Main26260.java
@@ -3,18 +3,12 @@ package acmicpc0212;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main26260 {
-	
+
 	static int[] arr;
-	static boolean[] visited;
 	static int n;
 
 	public static void main(String[] args) throws IOException {
@@ -23,48 +17,34 @@ public class Main26260 {
 		StringTokenizer st;
 
 		n = Integer.parseInt(br.readLine());
-		
-		ArrayList<Integer> list = new ArrayList<>();
 
-		Collections.sort(list);
-		
-		arr = new int[n+1];
-		visited = new boolean[n+1];
-		visited[0] = true;
-//		visited[n+1] = true;
-		
+		arr = new int[n + 1];
+
 		st = new StringTokenizer(br.readLine());
 		int minIndex = 0;
-		for(int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			int num = Integer.parseInt(st.nextToken());
 			arr[i] = num;
-			if(num == -1) {
+			if (num == -1) {
 				minIndex = i;
 			}
 		}
-		
+
 		int newNum = Integer.parseInt(br.readLine());
 		arr[minIndex] = newNum;
-		
+
 		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
-		
-		postOrder(1, (n+1)/2, n);
+
+		postOrder(1, (n + 1) / 2, n);
 
 	}
-	
+
 	static void postOrder(int start, int middle, int end) {
-		if(start >= end) {
+		if (start > end) {
 			return;
 		}
-//		if(visited[middle]) {
-//			return;
-//		}
-//		visited[middle] = true;
-//		if(middle - (middle+start)/2 >= 1)
-			postOrder(start, (middle+start)/2, middle-1);
-//		if(middle + (end - middle)/2 <= n)
-			postOrder(middle+1, (end + middle)/2, end);
+		postOrder(start, (middle - 1 + start) / 2, middle - 1);
+		postOrder(middle + 1, (end + middle + 1) / 2, end);
 		System.out.print(arr[middle] + " ");
 	}
 
